5b2e06ada0769660bb74a620f0bd5fe761023620
Chantal Hodge <chantal_h09@yahoo.com>
2022-10-02 10:59:20 -0500

    making sure git is up to date from saved work

---
+++ server.js
@@ -0,0 +1,27 @@
+const express= require("express");
+const path= require("path");
+const fs= require("fs");
+const util= require("util");
+const readfile= util.promisify(fs.readFile);
+const PORT= 3001;
+const app= express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"))
+function getNotes(){
+    return readfile("db/db.json", "utf-8").then(function(rawnotes){
+        let notesArray= [].concat(JSON.parse(rawnotes))
+        return notesArray
+    })
+}
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+})
+app.get("/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+})
+app.get("/api/notes", function(req, res){
+    getNotes().then(notes=>res.json(notes)).catch(err=>res.json (err))
+})
+app.listen(PORT, ()=>console.log("listening on"+ PORT))
