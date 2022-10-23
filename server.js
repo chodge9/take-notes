43e1c32b5cb1be7531e6819558423009bb2cd97c
Chantal Hodge <chantal_h09@yahoo.com>
2022-10-23 11:11:11 -0500

    added heroku port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path= require("path");
 const fs= require("fs");
 const util= require("util");
 const readfile= util.promisify(fs.readFile);
-const PORT= 3001;
+const PORT= process.env.PORT || 3001;
 const app= express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
